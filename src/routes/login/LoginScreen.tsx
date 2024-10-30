79d41aafd325e1e21a5ea4f3ef1f18fad798e217
lautaCuellar <lautilh69@gmail.com>
2024-10-30 17:30:45 -0300

    avanzando con el login
    coloque una imagen para el fondo puse el boton para ingresar, y los campos para colocar el usuario y la contraseña( el ojito ya esta funcionando)

--- src/routes/login/LoginScreen.tsx
+++ src/routes/login/LoginScreen.tsx
@@ -1,5 +1,9 @@
-
 import { Form, redirect } from "react-router-dom";
+import { useState } from 'react';
+import { Stack, TextField, Button, IconButton, InputAdornment } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import fondo from './loginFondo.png';
+
 
 
 export async function action () {
@@ -23,15 +27,72 @@ export async function action () {
 }
 
 export default function LoginScreen() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleClickShowPassword = () => setShowPassword(!showPassword);
     
     return (
-        <>
-            <h1>Login</h1>
-            <Form method='post'>
-                <button 
+    <Stack
+        spacing={10}
+        alignItems="center"
+        justifyContent="center"
+        sx={{ 
+            height: '100vh',
+            backgroundImage: `url(${fondo})`, // URL de tu imagen
+            backgroundSize: 'cover', // Ajusta la imagen para cubrir toda la pantalla
+    }}
+    >
+        <h1
+            style={{
+                
+                fontSize: "4rem" // Ajustar el tamaño de la fuente (puedes usar px, em, rem, etc.)
+            }}
+        >ReadApp</h1>
+
+        <Form 
+            method='post'
+            style={{
+                width: '80%', 
+                display: 'flex', 
+                flexDirection: 'column',
+                gap: '50px' 
+            }}
+        >
+            <TextField
+                label="Usuario" 
+                variant="outlined" 
+                type="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <TextField
+                label="Contraseña" 
+                variant="outlined" 
+                type={showPassword ? "text" : "password"}
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    fullWidth
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton onClick={handleClickShowPassword} edge="end">
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        )
+                    }}
+            />
+            
+            <Button 
                     type="submit"
-                >Loguearse</button>
+                    variant="contained"         
+                >Ingresar</Button>
             </Form>
-        </>
+    </Stack>
     )
 }
