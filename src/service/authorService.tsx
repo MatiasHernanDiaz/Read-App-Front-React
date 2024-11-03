01d41c2ba75a173da70c7d35578d9d7d5c0d72d4
gasabe <gabelardo@estudiantes.unsam.edu.ar>
2024-11-03 18:41:31 -0300

    correcion de errores

--- src/service/authorService.tsx
+++ src/service/authorService.tsx
@@ -1,14 +1,13 @@
 import axios from 'axios';
 import { Author } from '../model/Author';
 
-const URL: string = "http://localhost:9000";
-
+const URL: string = "http://localhost:9000"
 class AuthorService {
   async getAuthors({ name }: { name?: string }): Promise<Author[]> {
-    const authorJSON$ = await axios.get(`${URL}/authors?=${name || ''}`);
-    const authors = authorJSON$.data.map((authorJSON: Author) => Author.fromJSON(authorJSON));
-    return authors;
+    const authorJSON$ = await axios.get<Author[]>(`${URL}/authors?=${name || ''}`)
+    const authors = authorJSON$.data.map((authorJSON: Author) => Author.fromJSON(authorJSON))
+    return authors
   }
 }
 
-export const authorService = new AuthorService();
+export const authorService = new AuthorService()
