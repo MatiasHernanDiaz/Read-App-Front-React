81fa2eb4b4b9527fb9fc344c9369a45ec6899c19
Theo <narmontastheo@gmail.com>
2024-11-05 00:45:46 -0300

    coneccion backend

--- src/routes/book/BookComponent.tsx
+++ src/routes/book/BookComponent.tsx
@@ -9,9 +9,6 @@ import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 import "./BookComponent.css"
 
 
-const book: Book = new Book(1200, "el libro de bill", "https://http2.mlstatic.com/D_NQ_NP_729185-MLU77759079789_072024-O.webp", "alex hirsch", 300, new Date(1,2,2003), "español",30000,1)
-
-
 //function removeBook(): void {
  //   onDeleteBook.emit(this.book)
 //}
@@ -37,7 +34,7 @@ export default function BookComponent({book}:{book:Book}) {
                 <List className='comments'>
                     <ListItem className="comment"><AutoStoriesIcon/>{book.pages} Paginas</ListItem>
                     <ListItem className="comment"><TitleIcon/>{book.words} Palabras</ListItem>
-                    <ListItem className="comment"><CalendarMonthIcon/> {book.date.toDateString()}</ListItem>
+                    <ListItem className="comment"><CalendarMonthIcon/> {"dd/mm/aaa"/*book.date.toString()*/}</ListItem>
                     <ListItem className="comment"><TranslateIcon/>{book.lenguages}</ListItem>
                     <ListItem className="comment"><MonetizationOnIcon/>{book.sales} Ventas</ListItem>
                 </List>
--- src/routes/bookContainer/BookContainer.tsx
+++ src/routes/bookContainer/BookContainer.tsx
@@ -1,15 +1,12 @@
-import { List, Stack } from "@mui/material"
+import { Stack } from "@mui/material"
 import { Book } from "../../model/Book"
+import { bookService } from "../../services/bookService"
 import BookComponent from "../book/BookComponent"
 import './BookContainer.css'
 
-const books: Book[] = [
-    new Book(1200, "el libro de bill", "https://http2.mlstatic.com/D_NQ_NP_729185-MLU77759079789_072024-O.webp", "alex hirsch", 300, new Date(1,2,2003), "español",30000,1),
-    new Book(1200, "el libro de bill", "https://marketplace.canva.com/EAFjNCKkDPI/1/0/1003w/canva-portada-de-libro-de-fantas%C3%ADa-dram%C3%A1tico-verde-Ct1fLal3ekY.jpg", "alex hirsch", 300, new Date(1,2,2003), "español",30000,1),
-    new Book(1200, "el libro de bill", "https://http2.mlstatic.com/D_NQ_NP_729185-MLU77759079789_072024-O.webp", "alex hirsch", 300, new Date(1,2,2003), "español",30000,1),
-    new Book(1200, "el libro de bill", "https://marketplace.canva.com/EAFjNCKkDPI/1/0/1003w/canva-portada-de-libro-de-fantas%C3%ADa-dram%C3%A1tico-verde-Ct1fLal3ekY.jpg", "alex hirsch", 300, new Date(1,2,2003), "español",30000,1),
-    new Book(1200, "el libro de bill", "https://marketplace.canva.com/EAFjNCKkDPI/1/0/1003w/canva-portada-de-libro-de-fantas%C3%ADa-dram%C3%A1tico-verde-Ct1fLal3ekY.jpg", "alex hirsch", 300, new Date(1,2,2003), "español",30000,1)
-]
+const books: Book[] = await bookService.getBooks({
+    // name: text,
+   })
 
 export default function BookContainer() { 
     return(
