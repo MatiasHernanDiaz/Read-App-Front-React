136e61754955179b203cf9be15ef1bec98016d98
lautaCuellar <lautilh69@gmail.com>
2024-11-12 16:47:14 -0300

    cambio rutas
    modifique los endpoints del back, entonces los cambie aca para que la comunicacion sea correcta

--- src/services/loginService.tsx
+++ src/services/loginService.tsx
@@ -11,19 +11,19 @@ class LoginService {
     baseUrl = 'http://localhost:9000/auth/'
 
     async login( email: string, password: string ) {
-        const response = await axios.post<AuthResponse>( this.baseUrl + 'login', { email, password } )
+        const response = await axios.post<AuthResponse>( this.baseUrl + 'loginAdmin', { email, password } )
 
         return response.data;
     }
 
     async getSignedUser() {
-        const res = await axios.get<AuthResponse>('http://localhost:9000/auth/login')
+        const res = await axios.get<AuthResponse>('http://localhost:9000/auth/loginAdmin')
 
         return res.data.user
     }
 
     async logout() {
-        const response = await axios.get( this.baseUrl + 'logout' )
+        const response = await axios.get( this.baseUrl + 'logoutAdmin' )
 
         return response
     }
