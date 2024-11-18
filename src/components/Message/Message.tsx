3f8a10e25d1a874606ad6462ccd18bcf0d177a81
Diaz Matias Hernan <diaz.matiash@gmail.com>
2024-11-17 23:55:25 -0300

    arreglado tema de tipos

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,18 +1,25 @@
 import { Alert, AlertTitle } from "@mui/material";
-import { Msj } from "../../routes/app/MainFrame";
+import { AxiosResponse } from "axios";
 
 
 
-export default function Message({message} : {message:Msj}){
+export default function Message({res} : {res:AxiosResponse}){
     //severity = success, error, warnings
-    
+    const message = res.status >= 200 && res.status < 300 ? res.data : res.data.message
+    const setStatus = ()  => {
+        if(res.status >= 200 && res.status <=299){
+            return "success"
+        }
+        return "error"
+    }
+
 
     return(
         <>
-            {message.message && 
-                <Alert  variant="filled" severity={message.statusSeverity} sx={{position:"absolute", width:"80%", marginLeft:"5%",zIndex:50}}>
-                        <AlertTitle>{message.statusSeverity}</AlertTitle>
-                        <strong>{message.message}</strong> 
+            { res.data && 
+                <Alert  variant="filled" severity={setStatus()} sx={{position:"absolute", width:"80%", marginLeft:"5%",zIndex:50}}>
+                        <AlertTitle>{res.status}</AlertTitle>
+                        <strong>{message}</strong> 
                 </Alert>
             }
         </>
