92ea55d34019051733b84a512271dfa6e79d37e6
Diaz Matias Hernan <diaz.matiash@gmail.com>
2024-11-25 22:45:21 -0300

    test de Message

--- src/components/Message/Message.test.tsx
+++ src/components/Message/Message.test.tsx
@@ -1,15 +1,37 @@
-import { render, screen } from "@testing-library/react"
-import { describe, expect, it } from "vitest"
-import Message from "./Message"
-import { AxiosResponse } from "axios"
+import { act, cleanup, render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import Message from './Message'; // Asegúrate de ajustar la ruta
+import { AxiosResponse } from 'axios';
+
+describe('Message Component', () => {
+  it('muestra un mensaje de éxito cuando el status es 200', () => {
+    const mockResponse = {
+      status: 200,
+      data: 'Operación exitosa',
+    } as AxiosResponse; // Simula una respuesta de AxiosResponse
+
+    render(<Message res={mockResponse} />)
+
+    const alert = screen.getByTestId('alert')
+    
+
+    expect(alert.textContent).toBe('Operación exitosa')
+    expect(alert.className).toBe('MuiPaper-root MuiPaper-elevation MuiPaper-rounded MuiPaper-elevation0 MuiAlert-root MuiAlert-colorSuccess MuiAlert-filledSuccess MuiAlert-filled css-dzmomi-MuiPaper-root-MuiAlert-root'); // Verifica que sea un mensaje de éxito
+    cleanup()
+})
 
 
-describe('Message', () => {
-    it('Recibe un 200', () =>{
-        const res = {status: 200, data:{message:"test 200"}} as AxiosResponse
-        render(<Message res={res}/>)
-        const alert = screen.getByTestId('alert')
-        console.log(alert)
-        expect(alert.classList.contains('MuiAlert-filledSuccess')).toBe("MuiAlert-filledSuccess");
+  it('muestra un mensaje de error cuando el status es 500', () => {
+    const mockResponse2 = {
+      status: 500,
+      data: { message: 'Error del servidor' },
+    } as AxiosResponse;
+
+    render(<Message res={mockResponse2} />)
+    const alert = screen.getByTestId('alert')
+
+    expect(alert.textContent).toBe('Error del servidor')
+    expect(alert.className).toBe('MuiPaper-root MuiPaper-elevation MuiPaper-rounded MuiPaper-elevation0 MuiAlert-root MuiAlert-colorError MuiAlert-filledError MuiAlert-filled css-wo8dwg-MuiPaper-root-MuiAlert-root'); // Verifica que sea un mensaje de error
+    cleanup()
     })
 })
